5b42686ade3e4c849e5e9be8170028899f7a9958
usama <usamak.mirza@gmail.com>
2023-02-19 17:51:15 +0500

    front end completed

--- src/mixins/api_mixin.js
+++ src/mixins/api_mixin.js
@@ -22,10 +22,17 @@ export const api_mixin = {
         getTechStack() {
             axios.get(this.base_url + `api/v1/tech_stacks`)
                 .then(response => {
+                    console.log("API-- getTechStack")
                     this.tech_stacks = response.data.data.tech_stacks
+                    console.log(this.tech_stacks)
+                    console.log("-- getTechStack --")
+
                 })
                 .catch(e => {
+                    console.log("API Error-- getTechStack")
                     this.tech_stacks_errors.push(e)
+                    console.log( this.tech_stacks_errors)
+                    console.log("-- getTechStack --")
                 })
         },
 
